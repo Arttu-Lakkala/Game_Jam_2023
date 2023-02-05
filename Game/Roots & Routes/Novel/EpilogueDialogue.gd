1a9f2fb943b3115999a1ea0db4ad61d771747dae
Uriel30J <uriel.nyffenegger@outlook.com>
2023-02-05 14:36:14 +0200

    Added scenes

---
+++ Game/Roots & Routes/Novel/EpilogueDialogue.gd
@@ -0,0 +1,59 @@
+extends Node
+
+var dialogue_1 = [
+	{
+		"Name": "",
+"Text": "…Maybe it was good that you warned Eli not to expect too much. Hopefully, the cake will taste better than it looks."
+},{
+"Name": "",
+"Text": "You try not to worry too much about it. It’s out of your hands now; there is nothing left to do but twiddle your thumbs as you wait for the guests to arrive."
+},{
+"Name": "",
+"Text": "Your room is still sparsely decorated, but you do have a second-hand couch that a few of your friends helped you carry in. Eli is also going to keep the door to their room open, so there’s more space to hang out."
+},{
+"Name": "",
+"Text": "Their room is much nicer. Homely. Cosy. Perhaps yours will one day in the future have that same kind of feeling to it."
+},{
+"Name": "",
+"Text": "You can’t help but go through everything one more time. Snacks are on the table, drinks are cooling in the fridge, the cake is ready. But is that enough?"
+},{
+"Name": "You",
+"Text": "Do you think we should have gotten some tortilla chips as well?"
+},{
+"Name": "",
+"Text": "Eli gives you one of their looks. You’ve learned to interpret it as: You’re worrying too much again. And you probably are."
+},{
+"Name": "Eli",
+"Text": "I don’t think your friends will care too much about what kind of chips you serve. That’s not what this is about, right?"
+},{
+"Name": "",
+"Text": "They’re right, of course. You wouldn’t care about that kind of stuff if you were going to a friend’s place."
+},{
+"Name": "",
+"Text": "When the bell rings, you jump up from the couch and rush to open the door. Alex is the first to arrive. She greets you at the door with a broad grin on her face."
+},{
+"Name": "Alex",
+"Text": "Heyyy! I come bearing gifts."
+},{
+"Name": "",
+"Text": "She shows you the snacks she’s brought. As soon as she’s set them down on the table, she pulls you into a bear hug. You laugh and squeeze her back, comforted by her enthusiasm."
+},{
+"Name": "",
+"Text": "Soon, you’re showing her around the apartment. There’s not much to show, of course, but she’s excited nevertheless."
+},{
+"Name": "Alex",
+"Text": "You have a really nice place! Imagine green curtains here, and a plush armchair here, and a desk here…"
+},{
+"Name": "",
+"Text": "She goes on like that, helping you see the possibilities available to you even with your limited space, until other guests start to arrive and you’re distracted. But you think you will return to that conversation later."
+},{
+"Name": "",
+"Text": "Later, when you’re looking at your friends spread around the apartment, some sitting on the couch, some sitting by the window, some hanging out with Eli in their room…"
+},{
+"Name": "",
+"Text": "You think you might come to really like it here, after all. Maybe it’s not quite home yet, but it could be. So much of the city is still unknown to you…"
+},{
+"Name": "",
+"Text": "But that’s also exciting, isn’t it?"
+}
+]
